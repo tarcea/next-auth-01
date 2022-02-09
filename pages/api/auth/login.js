9982b7629705337fba040ed2e0999814954d7c66
tarcea <tarcea@gmail.com>
2022-02-09 13:43:07 +0100

    basic setup of the project, used an external API for auth trials

---
+++ pages/api/auth/login.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { serialize } from "cookie";
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.json({ message: "email and password are required" });
+    }
+    const response = await axios.post(
+      "http://localhost:5050/users/login",
+      req.body
+    );
+
+    const user = response.data;
+    const token = user.token;
+    const serialised = serialize("tokenJWT", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+      path: "/",
+    });
+    res.setHeader("Set-Cookie", serialised);
+    res.json({ message: "success", userName: user.username });
+  } catch (error) {
+    return res.json({ message: "please provide valid credentials." });
+  }
+};
+
+export default login;
